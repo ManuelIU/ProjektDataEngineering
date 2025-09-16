0053d1c3dee3e278ab9ff866205d85feb25fa48f
Manuel Gussmagg <manuel.gussmagg@iu-study.org>
2025-09-16 11:14:00 +0200

    create postgres db and store data

---
+++ postgres/init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS weather_data (
+    EventId          VARCHAR(50) PRIMARY KEY,
+    Type             VARCHAR(50),
+    Severity         VARCHAR(50),
+    StartTimeUTC     TIMESTAMP,
+    EndTimeUTC       TIMESTAMP,
+    PrecipitationIn  NUMERIC(5,2),
+    TimeZone         VARCHAR(50),
+    AirportCode      VARCHAR(10),
+    LocationLat      NUMERIC(9,6),
+    LocationLng      NUMERIC(9,6),
+    City             VARCHAR(100),
+    County           VARCHAR(100),
+    State            CHAR(2),
+    Zipcode          VARCHAR(10)
+);
